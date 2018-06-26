e6b83c3c644648aee5f7e9078d09fbe2dc5e8889
Nguyen Tan Dung <dung.nguyen@quant-edge.com>
2018-06-26 16:56:35 +0700

    fix router webhook

--- routes/webhookRouter.js
+++ routes/webhookRouter.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('webhook HIHI');
+});
+
+router.post('/', function(req, res, next) {
   console.log(req.body);
+  res.send('webhook HIHI');
 });
 
 module.exports = router;
